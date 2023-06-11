c67787a7a69e01d084922de2092bebf9a526de84
Torben <59419684+entorb@users.noreply.github.com>
2023-06-11 02:47:07 +0200

    stats minor

--- scripts/sync_stats_to_server.sh
+++ scripts/sync_stats_to_server.sh
@@ -5,4 +5,5 @@ script_dir=$(cd $(dirname $0) && pwd)
 cd $script_dir/..
 
 rsync -vhu stats.py entorb@entorb.net:html/strava/
+ssh entorb@entorb.net mkdir -p html/strava/download/123/stats-py/
 rsync -vhu download/123/stats-py/*.json entorb@entorb.net:html/strava/download/123/stats-py/
