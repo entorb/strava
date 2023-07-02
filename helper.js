c298993d8c1d72bf5d7a8d0e4967c728b24332e6
Torben <59419684+entorb@users.noreply.github.com>
2023-07-02 15:04:05 +0200

    helper_array_to_object_of_key_eq_value

--- helper.js
+++ helper.js
@@ -1,8 +1,20 @@
-// select: html element
-// options: map of key -> value pairs
-// def_value: default option
-// clear_first: clear prior to populating
-function helper_populate_select(select, options, def_value = null, clear_first = true) {
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+"use strict";
+
+/**
+ * Populate a select with options
+ * @param {Object} select html select element
+ * @param {Object} options object of key->value pairs
+ * @param {string} def_value default value
+ * @param {boolean} clear_first remove remove all existing values prior to populating
+ */
+function helper_populate_select(
+  select,
+  options,
+  def_value = null,
+  clear_first = true
+) {
   if (clear_first) {
     // remove all options
     select.innerHTML = "";
@@ -20,4 +32,16 @@ function helper_populate_select(select, options, def_value = null, clear_first =
     }
     select.appendChild(option);
   }
+}
+
+/**
+ * Populate a select with options
+ * @param {array} array  array/set
+ * @return {Object} object of map of key -> value pairs with key=value
+ */
+function helper_array_to_object_of_key_eq_value(array) {
+  return array.reduce((obj, value) => {
+    obj[value] = value;
+    return obj;
+  }, {});
 }
