c8f432bd5084175af24bf7479022e4d375ae40b4
Torben <59419684+entorb@users.noreply.github.com>
2023-07-02 16:05:04 +0200

    helper_tabulator_col_num and helper_tabulator_col_str

--- activityTable.js
+++ activityTable.js
@@ -2,263 +2,56 @@
 /* eslint-disable require-jsdoc */
 "use strict";
 
-// eslint-disable-next-line no-unused-vars
+const table_columns = [];
+
+table_columns.push(helper_tabulator_col_str("x_date", "Date"));
+table_columns.push(helper_tabulator_col_str("type", "Type"));
+table_columns.push(helper_tabulator_col_str("name", "Name", 120));
+table_columns.push(
+  helper_tabulator_col_str("x_nearest_city_start", "City", 120)
+);
+table_columns.push(
+  helper_tabulator_col_str("x_start_locality", "Known location start")
+);
+table_columns.push(helper_tabulator_col_str("x_end_locality", "-end"));
+table_columns.push(helper_tabulator_col_str("x_gear_name", "Gear", 120));
+table_columns.push(helper_tabulator_col_num("x_min", "Minutes"));
+table_columns.push(helper_tabulator_col_num("x_km", "Kilometers"));
+table_columns.push(helper_tabulator_col_num("km/h", "km/h"));
+table_columns.push(helper_tabulator_col_num("x_max_km/h", "max km/h"));
+table_columns.push(
+  helper_tabulator_col_num("total_elevation_gain", "Elevation (m)")
+);
+table_columns.push(
+  helper_tabulator_col_num("x_elev_m/km", "Elevation (m/km)")
+);
+table_columns.push(helper_tabulator_col_num("average_heartrate", "HR avg"));
+table_columns.push(helper_tabulator_col_num("max_heartrate", "HR max"));
+table_columns.push(helper_tabulator_col_num("average_cadence", "Cadence"));
+table_columns.push(helper_tabulator_col_num("average_watts", "Watts avg"));
+table_columns.push(helper_tabulator_col_num("kilojoules", "KJ"));
+table_columns.push(helper_tabulator_col_str("visibility", "visibility"));
+table_columns.push(helper_tabulator_col_num("athlete_count", "Athletes"));
+table_columns.push(helper_tabulator_col_num("workout_type", "W-Type"));
+table_columns.push(helper_tabulator_col_num("kudos_count", "Kudos"));
+table_columns.push(helper_tabulator_col_num("comment_count", "Comments"));
+table_columns.push(
+  helper_tabulator_col_num("achievement_count", "Achievements")
+);
+
 function defineTable() {
   const table = new Tabulator("#table-activity-list", {
     // height: "100%",
-    maxHeight: "100%", //do not let table get bigger than the height of its parent element
+    maxHeight: "100%", // do not let table get bigger than the height of its parent element
     // height: 800,
     layout: "fitDataStretch", // fit columns to width of table (optional)
-    tooltipsHeader: true,
-    //        selectable: false, // for row click
+    tooltipsHeader: false,
+    selectable: false, // for row click
+    columns: table_columns,
     initialSort: [{ column: "x_date", dir: "desc" }],
-    columns: [
-      // Define Table Columns
-      { title: "Date", field: "x_date", sorter: "string", headerFilter: true },
-      { title: "Type", field: "type", sorter: "string", headerFilter: true },
-      {
-        title: "Name",
-        field: "name",
-        sorter: "string",
-        headerFilter: true,
-        width: 120,
-      },
-      {
-        title: "City",
-        field: "x_nearest_city_start",
-        sorter: "string",
-        headerFilter: true,
-        width: 120,
-      },
-      {
-        title: "Known location start",
-        field: "x_start_locality",
-        sorter: "string",
-        headerFilter: true,
-      },
-      {
-        title: "-end",
-        field: "x_end_locality",
-        sorter: "string",
-        headerFilter: true,
-      },
-      {
-        title: "Gear",
-        field: "x_gear_name",
-        sorter: "string",
-        headerFilter: true,
-        width: 120,
-      },
-      {
-        title: "Minutes",
-        field: "x_min",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Kilometers",
-        field: "x_km",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "km/h",
-        field: "km/h",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "max km/h",
-        field: "x_max_km/h",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Elevation (m)",
-        field: "total_elevation_gain",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Elevation (m/km)",
-        field: "x_elev_m/km",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "HR avg",
-        field: "average_heartrate",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "HR max",
-        field: "max_heartrate",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Cadence",
-        field: "average_cadence",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Watts avg",
-        field: "average_watts",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "KJ",
-        field: "kilojoules",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Visible",
-        field: "visibility",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Athletes",
-        field: "athlete_count",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "W-Type",
-        field: "workout_type",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Kudos",
-        field: "kudos_count",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Comments",
-        field: "comment_count",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-      {
-        title: "Achievements",
-        field: "achievement_count",
-        sorter: "number",
-        headerFilter: "number",
-        hozAlign: "right",
-        sorterParams: {
-          alignEmptyValues: "bottom",
-        },
-        headerFilterPlaceholder: "filter >=",
-        headerFilterFunc: ">=",
-      },
-    ],
   });
-
   return table;
 }
-
 const table = defineTable();
 
 // wait for tableBuilt event and set data afterwards
@@ -268,6 +61,7 @@ table.on("tableBuilt", () => {
   );
 });
 
+// row click event -> open Strava
 table.on("cellClick", (e, cell) => {
   const row = cell.getRow();
   const rowData = row.getData();
