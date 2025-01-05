79eef769effa40cf036d1885e8c6eebfa20521a7
Torben <59419684+entorb@users.noreply.github.com>
2025-01-05 07:34:17 +0100

    Update sync_to_server.sh

--- scripts/sync_to_server.sh
+++ scripts/sync_to_server.sh
@@ -4,7 +4,8 @@
 script_dir=$(cd $(dirname $0) && pwd)
 cd $script_dir/..
 
+rsync -vhu *.html entorb@entorb.net:html/strava/
 rsync -vhu *.pl entorb@entorb.net:html/strava/
 rsync -vhu TMsStrava.pm entorb@entorb.net:html/strava/
-rsync -vhu lib/* entorb@entorb.net:html/strava/lib/
 rsync -vhu activityStats2.* entorb@entorb.net:html/strava/
+rsync -vhu lib/* entorb@entorb.net:html/strava/lib/
